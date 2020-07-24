e3c4a38d232b105c14db27179288eb1d280a0279
eagarciaf <eduabab.boy@gmail.com>
2020-07-24 17:42:42 -0600

    test desde eclipse

--- pruebascompi/Pruebascompi.java
+++ pruebascompi/Pruebascompi.java
@@ -16,6 +16,7 @@ public class Pruebascompi {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    	//test desde eclipse
         ventana v = new ventana();
         v.show();
     }
